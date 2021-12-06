08763512cdd98a5772a4596d8e771a7e252946a1
miNimaL593 <minimal593@gmail.com>
2021-12-06 14:35:29 +0700

    block information

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function (){
         adaptiveHeight: false,
         slidesToShow: 1,
         speed: 1000,
-        autoplay: true,
+        autoplay: false,
         touchThreshold: 10,
         autoplaySpeed: 2000,
 
